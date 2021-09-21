4f802b3c0a3c4814240facfdcf109e5f0bb746ee
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2021-09-21 00:14:24 -0300

    Updated code format

--- include/logger.hpp
+++ include/logger.hpp
@@ -1,14 +1,15 @@
 #ifndef _DIG_LOGGER_HPP_
 #define _DIG_LOGGER_HPP_ 1
 
-#if defined(DIG_LOGGER_STATIC) || !(defined(_MSC_VER) || defined(_WIN32) || defined (_WIN64))
-  #define _DIG_LOGGER_API_ 
+#if defined(DIG_LOGGER_STATIC) || \
+    !(defined(_MSC_VER) || defined(_WIN32) || defined(_WIN64))
+#define _DIG_LOGGER_API_
 #else
-  #ifdef _DIG_LOGGER_COMPILE_TIME_
-    #define _DIG_LOGGER_API_ __declspec(dllexport)
-  #else
-    #define _DIG_LOGGER_API_ __declspec(dllimport)
-  #endif
+#ifdef _DIG_LOGGER_COMPILE_TIME_
+#define _DIG_LOGGER_API_ __declspec(dllexport)
+#else
+#define _DIG_LOGGER_API_ __declspec(dllimport)
+#endif
 #endif
 
 #include <string>
@@ -16,40 +17,47 @@
 namespace DIG {
 namespace Logger {
 
-typedef enum { none,error,warning,info,debug,trace } Type;
+typedef enum { none, error, warning, info, debug, trace } Type;
 void _DIG_LOGGER_API_ setFileLogLevel(const char);
 void _DIG_LOGGER_API_ setScreenLogLevel(const char);
 void _DIG_LOGGER_API_ msg(const char*, const unsigned, const char*, const Type);
-void _DIG_LOGGER_API_ msg(const char*, const unsigned, const std::string, const Type);
+void _DIG_LOGGER_API_ msg(const char*,
+                          const unsigned,
+                          const std::string,
+                          const Type);
 
-}}
+}  // namespace Logger
+}  // namespace DIG
 
 #ifndef DIG_LOGGER_DISABLE
 
-  #define SETSCREENLOGLEVEL(a) DIG::Logger::setScreenLogLevel(a)
-  #define SETFILELOGLEVEL(a) DIG::Logger::setFileLogLevel(a)
-  #ifdef DIG_LOGGER_NO_DEBUG
-    #define LOG_TRACE(a) 
-    #define LOG_DEBUG(a) 
-  #else
-    #define LOG_TRACE(a) DIG::Logger::msg(__FILE__,__LINE__, a ,DIG::Logger::trace)
-    #define LOG_DEBUG(a) DIG::Logger::msg(__FILE__,__LINE__, a ,DIG::Logger::debug)
-  #endif
-  #define LOG_ERROR(a) DIG::Logger::msg(__FILE__,__LINE__, a ,DIG::Logger::error)
-  #define LOG_WARN(a) DIG::Logger::msg(__FILE__,__LINE__, a ,DIG::Logger::warning)
-  #define LOG_WARNING(a) DIG::Logger::msg(__FILE__,__LINE__, a ,DIG::Logger::warning)
-  #define LOG_INFO(a) DIG::Logger::msg(__FILE__,__LINE__, a ,DIG::Logger::info)
+// clang-format off
+#define SETSCREENLOGLEVEL(a) DIG::Logger::setScreenLogLevel(a)
+#define SETFILELOGLEVEL(a) DIG::Logger::setFileLogLevel(a)
+#define LOG_ERROR(a) DIG::Logger::msg(__FILE__, __LINE__, a, DIG::Logger::error)
+#define LOG_WARN(a) DIG::Logger::msg(__FILE__, __LINE__, a, DIG::Logger::warning)
+#define LOG_WARNING(a) DIG::Logger::msg(__FILE__, __LINE__, a, DIG::Logger::warning)
+#define LOG_INFO(a) DIG::Logger::msg(__FILE__, __LINE__, a, DIG::Logger::info)
+// clang-format on
+
+#ifdef DIG_LOGGER_NO_DEBUG
+#define LOG_TRACE(a)
+#define LOG_DEBUG(a)
+#else
+#define LOG_TRACE(a) DIG::Logger::msg(__FILE__, __LINE__, a, DIG::Logger::trace)
+#define LOG_DEBUG(a) DIG::Logger::msg(__FILE__, __LINE__, a, DIG::Logger::debug)
+#endif
 
 #else
 
-  #define SETSCREENLOGLEVEL(a) 
-  #define SETFILELOGLEVEL(a) 
-  #define LOG_TRACE(a) 
-  #define LOG_DEBUG(a) 
-  #define LOG_ERROR(a) 
-  #define LOG_WARN(a) 
-  #define LOG_WARNING(a) 
-  #define LOG_INFO(a) 
+#define SETSCREENLOGLEVEL(a)
+#define SETFILELOGLEVEL(a)
+#define LOG_TRACE(a)
+#define LOG_DEBUG(a)
+#define LOG_ERROR(a)
+#define LOG_WARN(a)
+#define LOG_WARNING(a)
+#define LOG_INFO(a)
 
 #endif
 
--- src/logger.cpp
+++ src/logger.cpp
@@ -1,27 +1,28 @@
 #include "logger.hpp"
 
-#if defined(_MSC_VER) || defined(_WIN32) || defined (_WIN64)
+#if defined(_MSC_VER) || defined(_WIN32) || defined(_WIN64)
 #include <windows.h>
 #endif
 
+#include <chrono>
 #include <fstream>
-#include <string>
-#include <sstream>
 #include <iomanip>
 #include <iostream>
-#include <thread>
 #include <mutex>
-#include <chrono>
+#include <sstream>
+#include <string>
+#include <thread>
 
 #ifdef DIG_LOGGER_SCREEN_NO_FILE
-  #define DIG_LOGGER_FILE 
+#define DIG_LOGGER_FILE
 #else
-  #define DIG_LOGGER_FILE << f << " " << std::setfill('0') << std::setw(4) << l << ":"
+#define DIG_LOGGER_FILE \
+  << f << " " << std::setfill('0') << std::setw(4) << l << ":"
 #endif
 #ifdef DIG_LOGGER_SCREEN_NO_THREAD
-  #define DIG_LOGGER_THREAD 
+#define DIG_LOGGER_THREAD
 #else
-  #define DIG_LOGGER_THREAD << std::this_thread::get_id() << ":"
+#define DIG_LOGGER_THREAD << std::this_thread::get_id() << ":"
 #endif
 
 namespace DIG {
@@ -34,14 +35,38 @@ std::mutex mtx;
 
 std::ofstream file;
 
-void setFileLogLevel(const char v){
+void setFileLogLevel(const char v) {
   fll = (uint8_t)v;
-  if(v == none){
+  if (v == none) {
     file.close();
-  }else{
+  } else {
     file.open("log.htm");
-    if(file.good()){
-      file<<"<!DOCTYPE html><head><title>Log</title><style>body { background:#000; color:ccc; font-family:Verdana; font-size:12px; }table { width: 100%; border:1px dashed #666; background:#444; cellpadding:0; cellspacing:0; }tr:hover {background:#555;}td {padding:1px; color:#aaa; border-right:1px solid #666; border-bottom:1px solid #666;}th {color:#666;border-bottom:1px solid #666;}.level_"<<error   <<" .message:before {content:\"Error: \";color:#f00;}.level_"<<error   <<" td {color:#f00;}.level_"<<warning <<" .message:before {content:\"Warning: \";color:#fa0;}.level_"<<warning <<" td {color:#fa0;}.level_"<<debug     <<" .message:before {content:\"Debug: \";color:#aaa;}.level_"<<debug     <<" td {color:#aaa;}.level_"<<trace   <<" .message:before {content:\"Trace: \";color:#666;}.level_"<<trace   <<" td {color:#666;}.level_"<<info    <<" .message:before {content:\"Information: \";color:#66f;}.level_"<<info    <<" td {color:#66f;}.time {width:130px;text-align:right;}.file {width:200px;}.line {text-align:right;width:50px;}th {text-align:center;}.message {}</style><body><table cellpadding=0 cellspacing=0><tr><th class='time'>time</th><th class='line'>thread</th><th class='line'>line</th><th class='file'>File</th><th class='message'>Message</th></tr>";
+    if (file.good()) {
+      file << "<!DOCTYPE html><head><title>Log</title><style>body { "
+              "background:#000; color:ccc; font-family:Verdana; "
+              "font-size:12px; }table { width: 100%; border:1px dashed #666; "
+              "background:#444; cellpadding:0; cellspacing:0; }tr:hover "
+              "{background:#555;}td {padding:1px; color:#aaa; border-right:1px "
+              "solid #666; border-bottom:1px solid #666;}th "
+              "{color:#666;border-bottom:1px solid #666;}.level_"
+           << error
+           << " .message:before {content:\"Error: \";color:#f00;}.level_"
+           << error << " td {color:#f00;}.level_" << warning
+           << " .message:before {content:\"Warning: \";color:#fa0;}.level_"
+           << warning << " td {color:#fa0;}.level_" << debug
+           << " .message:before {content:\"Debug: \";color:#aaa;}.level_"
+           << debug << " td {color:#aaa;}.level_" << trace
+           << " .message:before {content:\"Trace: \";color:#666;}.level_"
+           << trace << " td {color:#666;}.level_" << info
+           << " .message:before {content:\"Information: \";color:#66f;}.level_"
+           << info
+           << " td {color:#66f;}.time {width:130px;text-align:right;}.file "
+              "{width:200px;}.line {text-align:right;width:50px;}th "
+              "{text-align:center;}.message {}</style><body><table "
+              "cellpadding=0 cellspacing=0><tr><th class='time'>time</th><th "
+              "class='line'>thread</th><th class='line'>line</th><th "
+              "class='file'>File</th><th class='message'>Message</th></tr>";
+      // clang-format off
       /* unoptmized
       Q(output)<<"<!DOCTYPE html><head><title>Log</title><style>";
       Q(output)<<"body { background:#000; color:ccc; font-family:Verdana; font-size:12px; }";
@@ -65,83 +90,115 @@ void setFileLogLevel(const char v){
       Q(output)<<"th {text-align:center;}.message {}";
       Q(output)<<"</style><body><table cellpadding=0 cellspacing=0><tr><th class='time'>time</th><th class='line'>line</th><th class='file'>File</th><th class='message'>Message</th></tr>";
       */
+      // clang-format on
     }
   }
 }
 
-void setScreenLogLevel(const char v){
+void setScreenLogLevel(const char v) {
   sll = (uint8_t)v;
 }
 
-inline std::string formatTime(const std::chrono::milliseconds time){
+inline std::string formatTime(const std::chrono::milliseconds time) {
   std::stringstream SS;
-  SS << std::setfill('0') << std::setw(2) << time.count()/(1000*60*60);
-  unsigned T = time.count()%(1000*60*60);
-  SS << ":" << std::setfill('0') << std::setw(2) << T/(1000*60);
-  T%=1000*60;
-  SS << ":" << std::setfill('0') << std::setw(2) << T/1000;
-  SS << "." << std::setfill('0') << std::setw(3) << T%1000;
+  SS << std::setfill('0') << std::setw(2) << time.count() / (1000 * 60 * 60);
+  unsigned T = time.count() % (1000 * 60 * 60);
+  SS << ":" << std::setfill('0') << std::setw(2) << T / (1000 * 60);
+  T %= 1000 * 60;
+  SS << ":" << std::setfill('0') << std::setw(2) << T / 1000;
+  SS << "." << std::setfill('0') << std::setw(3) << T % 1000;
   return SS.str();
-  
-  //return std::put_time(std::ctime(),)
+
+  // return std::put_time(std::ctime(),)
 }
 
-void msg(const char* f, const unsigned l, const char* s, const Type t){
-  std::chrono::milliseconds diffTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now()-start);
+void msg(const char* f, const unsigned l, const char* s, const Type t) {
+  std::chrono::milliseconds diffTime =
+      std::chrono::duration_cast<std::chrono::milliseconds>(
+          std::chrono::system_clock::now() - start);
   mtx.lock();
-  if(t <= sll){
-    #define P std::cout << "[" << formatTime(diffTime) << "]" DIG_LOGGER_THREAD DIG_LOGGER_FILE << " " << s << std::endl;
-    #if defined(_MSC_VER) || defined(_WIN32) || defined (_WIN64)
-      HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-      SetConsoleTextAttribute(hConsole, (t == Logger::error)?12:((t==Logger::warning)?14:((t==Logger::info)?11:(t==Logger::debug?7:8))));
-      P;
-      SetConsoleTextAttribute(hConsole, 7);
-    #else
-      if(t == error)
-        std::cout << "\x1b[31m";
-      else if(t == warning)
-        std::cout << "\x1b[1;33m";
-      else if(t == info)
-        std::cout << "\x1b[0;36m";
-      else if(t == debug)
-        std::cout << "\x1B[1;37m";
-      P;
-      std::cout << "\x1B[0m";
-    #endif
+  if (t <= sll) {
+#define P                                  \
+  std::cout << "[" << formatTime(diffTime) \
+            << "]" DIG_LOGGER_THREAD DIG_LOGGER_FILE << " " << s << std::endl;
+#if defined(_MSC_VER) || defined(_WIN32) || defined(_WIN64)
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(
+        hConsole,
+        (t == Logger::error)
+            ? 12
+            : ((t == Logger::warning)
+                   ? 14
+                   : ((t == Logger::info) ? 11
+                                          : (t == Logger::debug ? 7 : 8))));
+    P;
+    SetConsoleTextAttribute(hConsole, 7);
+#else
+    if (t == error)
+      std::cout << "\x1b[31m";
+    else if (t == warning)
+      std::cout << "\x1b[1;33m";
+    else if (t == info)
+      std::cout << "\x1b[0;36m";
+    else if (t == debug)
+      std::cout << "\x1B[1;37m";
+    P;
+    std::cout << "\x1B[0m";
+#endif
   }
-  if((t <= fll)&&(file.good())){
-    file<<"<tr class='level_"<<(int)t<<"'><td class='time'>" << formatTime(diffTime) << "</td><td class='line'>"<<std::this_thread::get_id()<<"</td><td class='line'>"<<l<<"</td><td class='file'>"<<f<<"</td><td class='message'>"<<s<<"</td></tr>"<<std::endl;
+  if ((t <= fll) && (file.good())) {
+    file << "<tr class='level_" << (int)t << "'><td class='time'>"
+         << formatTime(diffTime) << "</td><td class='line'>"
+         << std::this_thread::get_id() << "</td><td class='line'>" << l
+         << "</td><td class='file'>" << f << "</td><td class='message'>" << s
+         << "</td></tr>" << std::endl;
   }
   mtx.unlock();
 }
 
-void msg(const char* f, const unsigned l, const std::string s, const Type t){
-  std::chrono::milliseconds diffTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now()-start);
+void msg(const char* f, const unsigned l, const std::string s, const Type t) {
+  std::chrono::milliseconds diffTime =
+      std::chrono::duration_cast<std::chrono::milliseconds>(
+          std::chrono::system_clock::now() - start);
   mtx.lock();
-  if(t <= sll){
-    #define P std::cout << "[" << formatTime(diffTime) << "]" DIG_LOGGER_THREAD DIG_LOGGER_FILE << " " << s << std::endl;
-    #if defined(_MSC_VER) || defined(_WIN32) || defined (_WIN64)
-      HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-      SetConsoleTextAttribute(hConsole, (t == Logger::error)?12:((t==Logger::warning)?14:((t==Logger::info)?11:(t==Logger::debug?7:8))));
-      P;
-      SetConsoleTextAttribute(hConsole, 7);
-    #else
-      if(t == error)
-        std::cout << "\x1b[31m";
-      else if(t == warning)
-        std::cout << "\x1b[1;33m";
-      else if(t == info)
-        std::cout << "\x1b[0;36m";
-      else if(t == debug)
-        std::cout << "\x1B[1;37m";
-      P;
-      std::cout << "\x1B[0m";
-    #endif
+  if (t <= sll) {
+#define P                                  \
+  std::cout << "[" << formatTime(diffTime) \
+            << "]" DIG_LOGGER_THREAD DIG_LOGGER_FILE << " " << s << std::endl;
+#if defined(_MSC_VER) || defined(_WIN32) || defined(_WIN64)
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(
+        hConsole,
+        (t == Logger::error)
+            ? 12
+            : ((t == Logger::warning)
+                   ? 14
+                   : ((t == Logger::info) ? 11
+                                          : (t == Logger::debug ? 7 : 8))));
+    P;
+    SetConsoleTextAttribute(hConsole, 7);
+#else
+    if (t == error)
+      std::cout << "\x1b[31m";
+    else if (t == warning)
+      std::cout << "\x1b[1;33m";
+    else if (t == info)
+      std::cout << "\x1b[0;36m";
+    else if (t == debug)
+      std::cout << "\x1B[1;37m";
+    P;
+    std::cout << "\x1B[0m";
+#endif
   }
-  if((t <= fll)&&(file.good())){
-    file<<"<tr class='level_"<<(int)t<<"'><td class='time'>" << formatTime(diffTime) << "</td><td class='line'>"<<std::this_thread::get_id()<<"</td><td class='line'>"<<l<<"</td><td class='file'>"<<f<<"</td><td class='message'>"<<s<<"</td></tr>"<<std::endl;
+  if ((t <= fll) && (file.good())) {
+    file << "<tr class='level_" << (int)t << "'><td class='time'>"
+         << formatTime(diffTime) << "</td><td class='line'>"
+         << std::this_thread::get_id() << "</td><td class='line'>" << l
+         << "</td><td class='file'>" << f << "</td><td class='message'>" << s
+         << "</td></tr>" << std::endl;
   }
   mtx.unlock();
 }
 
-}}
+}  // namespace Logger
+}  // namespace DIG
