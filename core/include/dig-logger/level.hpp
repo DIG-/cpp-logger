77ffaaf5b955abdd609b1dd30cb290c9f96367ed
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2023-07-02 12:35:39 -0300

    Reordered Logger::Level

--- core/include/dig-logger/level.hpp
+++ core/include/dig-logger/level.hpp
@@ -3,7 +3,7 @@
 namespace DIG {
 namespace Logger {
 
-enum class Level { ASSERT, ERROR, WARNING, INFORMATION, DEBUG, VERBOSE, NONE };
+enum class Level { NONE, VERBOSE, DEBUG, INFORMATION, WARNING, ERROR, ASSERT };
 
 }  // namespace Logger
 }  // namespace DIG
