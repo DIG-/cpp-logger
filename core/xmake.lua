b41b518a3216b3bdc822d9f628b57e9f18a9ca9a
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2023-07-02 19:42:54 -0300

    Update xmake to try to merge libraries

--- core/xmake.lua
+++ core/xmake.lua
@@ -2,5 +2,6 @@ add_rules("mode.debug", "mode.release")
 set_languages("c++20")
 
 target("core")
+    set_default(false)
     set_kind("static")
     add_includedirs("include", {public=true})
--- xmake.lua
+++ xmake.lua
@@ -1,10 +1,14 @@
 add_rules("mode.debug", "mode.release")
+set_project("dig-logger")
+set_version("2.0.0")
 set_languages("c++20")
 
 target("dig-logger")
+    add_rules("utils.merge.archive")
+    set_kind("static")
     add_deps("core", {public=true})
     add_deps("util-filter", {public=true})
-    set_kind("static")
+    set_policy("build.merge_archive", true)
 
 includes("core")
 includes("util/filter")
