44521eba292c39b9342b79e3ab60da55ea491f6d
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2023-07-02 21:47:27 -0300

    Configured to merge build library

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <dig-logger/logger.hpp>
+#include <dig-logger/util/filter.hpp>
 
 int main(unsigned argc, char**argv){
+    DIG::Logger::Util::Filter filter(nullptr, DIG::Logger::Level::VERBOSE);
+    filter.log(DIG::Logger::Level::VERBOSE,"a",std::optional<std::exception>(),"",std::source_location::current());
     std::cout << "Run" << std::endl;
     return 0;
 }
--- xmake.lua
+++ xmake.lua
@@ -4,10 +4,10 @@ set_version("2.0.0")
 set_languages("c++20")
 
 target("dig-logger")
-    add_rules("utils.merge.archive")
     set_kind("static")
     add_deps("core", {public=true})
     add_deps("util-filter", {public=true})
+    add_files("core/src/dummy.cpp")
     set_policy("build.merge_archive", true)
 
 includes("core")
