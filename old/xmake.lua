9628e2987b395430a1e3f27afdfa00506e66689e
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2023-07-04 19:27:30 -0300

    Removed old code

--- old/xmake.lua
+++
@@ -1,8 +0,0 @@
-add_rules("mode.debug", "mode.release")
-
-target("DIG-Logger")
-    set_kind("static")
-    set_version("1.1.1")
-    add_defines("DIG_LOGGER_STATIC")
-    add_includedirs("include", {public = true})
-    add_files("src/*.cpp")
