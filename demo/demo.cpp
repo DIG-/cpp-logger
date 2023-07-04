9e69b93521669d4830acf520d94799b617c6100d
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2023-07-04 19:09:05 -0300

    Updated demo

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -1,13 +1,19 @@
 #include <dig-logger/logger.hpp>
+#include <dig-logger/printer/html.hpp>
+#include <dig-logger/printer/plain.hpp>
+#include <dig-logger/printer/stdout.hpp>
 #include <dig-logger/util/filter.hpp>
 #include <dig-logger/util/union.hpp>
-#include <dig-logger/printer/stdout.hpp>
 #include <iostream>
 
-
 int main(int argc, char** argv) {
-  std::unique_ptr<DIG::Logger::LoggerInterface> printer = std::make_unique<DIG::Logger::Printer::StdOut>();
-  DIG::Logger::Logger logger(printer,"demo");
+  std::unique_ptr<DIG::Logger::LoggerInterface> p_stdout = std::make_unique<DIG::Logger::Printer::StdOut>();
+  std::unique_ptr<DIG::Logger::LoggerInterface> p_plain = std::make_unique<DIG::Logger::Printer::Plain>("demo.txt");
+  std::unique_ptr<DIG::Logger::LoggerInterface> p_html = std::make_unique<DIG::Logger::Printer::Html>("demo.htm");
+  std::array<std::unique_ptr<DIG::Logger::LoggerInterface>, 3> printers = {std::move(p_stdout), std::move(p_plain),
+                                                                           std::move(p_html)};
+  std::unique_ptr<DIG::Logger::LoggerInterface> printer = std::make_unique<DIG::Logger::Util::Union<3>>(printers);
+  DIG::Logger::Logger logger(printer, "demo");
   logger.v("verbose test");
   logger.d("debug test");
   logger.i("info test");
