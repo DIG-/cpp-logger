eabbafe2cce60acdb8fac74a56761644f4fd1e9e
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2023-07-06 16:17:33 -0300

    Demo with compacted code

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -7,13 +7,12 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-  std::unique_ptr<DIG::Logger::LoggerInterface> p_stdout = std::make_unique<DIG::Logger::Printer::StdOut>();
-  std::unique_ptr<DIG::Logger::LoggerInterface> p_plain = std::make_unique<DIG::Logger::Printer::Plain>("demo.txt");
-  std::unique_ptr<DIG::Logger::LoggerInterface> p_html = std::make_unique<DIG::Logger::Printer::Html>("demo.htm");
-  std::array<std::unique_ptr<DIG::Logger::LoggerInterface>, 3> printers = {std::move(p_stdout), std::move(p_plain),
-                                                                           std::move(p_html)};
-  // auto printer = std::make_shared<DIG::Logger::Util::Union<3>>(printers);
-  DIG::Logger::Logger logger(std::make_shared<DIG::Logger::Util::Union<3>>(printers), "demo");
+  auto p_stdout = std::make_unique<DIG::Logger::Printer::StdOut>();
+  auto p_plain = std::make_unique<DIG::Logger::Printer::Plain>("demo.txt");
+  auto p_html = std::make_unique<DIG::Logger::Printer::Html>("demo.htm");
+
+  DIG::Logger::Logger logger(std::make_shared<DIG::Logger::Util::Union<3>>(p_stdout, p_plain, p_html), "demo");
+
   logger.v("verbose test");
   logger.d("debug test");
   logger.i("info test");
