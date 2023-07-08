8e1807d12ae8334410479465be6c79c7057e1e99
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2023-07-08 13:20:06 -0300

    Caalling Appveyor api only one time per test

--- test/appveyor.ps1
+++ test/appveyor.ps1
@@ -1,19 +1,18 @@
 $target = $args[0]
-Add-AppveyorTest -Name "$target" -Framework "Custom" -Filename "$target" -Outcome Running
 $start = [Math]::Round((Get-Date).ToFileTimeUTC() / 10000)
 
 xmake b "$target"
 if ( ! $? ) {
     $time = [Math]::Round((Get-Date).ToFileTimeUTC() / 10000) - $start
-    Update-AppveyorTest -Name "$target" -Outcome Failed -Duration $time -ErrorMessage "Failed to build"
+    Add-AppveyorTest -Name "$target" -Framework "Custom" -Filename "$target" -Outcome Failed -Duration $time -ErrorMessage "Failed to build"
     exit
 }
 xmake r "$target"
 if ( ! $? ) {
     $time = [Math]::Round((Get-Date).ToFileTimeUTC() / 10000) - $start
-    Update-AppveyorTest -Name "$target" -Outcome Failed -Duration $time -ErrorMessage "Error running test"
+    Add-AppveyorTest -Name "$target" -Framework "Custom" -Filename "$target" -Outcome Failed -Duration $time -ErrorMessage "Error running test"
     exit
 }
 
 $time = [Math]::Round((Get-Date).ToFileTimeUTC() / 10000) - $start
-Update-AppveyorTest -Name "$target" -Outcome Passed -Duration $time
+Add-AppveyorTest -Name "$target" -Framework "Custom" -Filename "$target" -Outcome Passed -Duration $time
