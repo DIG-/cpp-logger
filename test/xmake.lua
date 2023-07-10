b4291400c6f8794746a3e00681de77f1f50cf3e0
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2023-07-10 11:36:20 -0300

    Created logger propagation test

--- test/xmake.lua
+++ test/xmake.lua
@@ -1,6 +1,14 @@
 add_rules("mode.debug", "mode.release")
 set_languages("c++20")
 
+target("test-core-logger")
+    set_default(false)
+    set_kind("binary")
+    set_group("test")
+    set_optimize("none")
+    add_deps("dig-logger")
+    add_files("src/core/logger.cpp")
+
 target("test-filter-forwarding")
     set_default(false)
     set_kind("binary")
