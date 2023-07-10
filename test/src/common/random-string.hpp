88a49d065b8b73da6dc6fe7ea4057fe29423e47d
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2023-07-10 17:52:57 -0300

    Returned random string code

--- test/src/common/random-string.hpp
+++ test/src/common/random-string.hpp
@@ -2,16 +2,16 @@
 
 #include <algorithm>
 #include <string>
-/*
+
 const char random_charset[] =
     "0123456789"
     "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     "abcdefghijklmnopqrstuvwxyz";
 
 const char random_char() { return random_charset[rand() % (sizeof(random_charset) - 1)]; }
-*/
+
 std::string random_string(size_t length) {
-  //std::string str(length, 0);
-  //std::generate_n(str.begin(), length, ::random_char);
-  return "bobosbdabklasjdd,bkjbjh";
+  std::string str(length, 0);
+  std::generate_n(str.begin(), length, ::random_char);
+  return str;
 }
