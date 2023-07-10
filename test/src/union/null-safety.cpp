edb94b9eadf5f4eb920664745225065895006b57
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2023-07-10 10:28:13 -0300

    Created null safety test for Union

---
+++ test/src/union/null-safety.cpp
@@ -0,0 +1,22 @@
+#include <dig-logger/printer/stdout.hpp>
+#include <dig-logger/util/union.hpp>
+#include <stdexcept>
+
+using namespace DIG::Logger;
+
+int main(int argc, char** argv) {
+  try {
+    Util::Union<2> test1(std::unique_ptr<LoggerInterface>(nullptr), std::unique_ptr<LoggerInterface>(nullptr));
+  } catch (std::runtime_error& error) {
+    try {
+      Util::Union<2> test2(std::unique_ptr<Printer::StdOut>(), std::unique_ptr<LoggerInterface>(nullptr));
+    } catch (std::runtime_error& error) {
+      try {
+        Util::Union<2> test3(std::unique_ptr<LoggerInterface>(nullptr), std::unique_ptr<Printer::StdOut>());
+      } catch (std::runtime_error& error) {
+        return 0;
+      }
+    }
+  }
+  return -1;
+}
