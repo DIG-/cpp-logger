0928031eb115c9f5b7d60faa62afb92a3fdba7c1
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2023-07-18 10:56:27 -0300

    Added version 2.1.0 to package

--- repo/packages/d/dig-logger/xmake.lua
+++ repo/packages/d/dig-logger/xmake.lua
@@ -6,6 +6,8 @@ package("dig-logger")
     add_urls("https://github.com/DIG-/cpp-logger/archive/refs/tags/$(version).zip", {alias="zip"})
     add_urls("https://github.com/DIG-/cpp-logger.git", {alias="git"})
 
+    add_versions("zip:2.1.0", "f5acb24bf20a04fb27d2209f12e35a853ce5e0957d73be905272f8101a435f55")
+    add_versions("git:2.1.0", "f39628571574f4d256a3905a00856c47dc82d11d")
     add_versions("zip:2.0.1", "6a0034f410b969b6d88ef260727d52ef70a4b4b46003179d74ad96339a1568ed")
     add_versions("git:2.0.1", "123ad0f4a90c1ca64f0fc170cb9bc3e6a06718d6")
     add_versions("zip:2.0.0", "4535ef2b07554631903604ca20153af81cb7175e7faa169082fd17e405ad51f2")
